04f406466db7bd230a74e4f050705b96fcc02343
Gustavo Aragão <gustavoma_@hotmail.com>
2022-02-08 11:00:27 -0300

    forbid creation of existent categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
+    const categoryAlreadyExist = categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExist) {
+        return response.status(400).json({ error: "Category Already Exists" });
+    }
+
     categoriesRepository.create({ name, description });
 
     return response.status(201).send();
