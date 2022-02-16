0a46914c4f186597ee8f9afe1eca213b7df0f402
Gustavo Aragão <gustavoma_@hotmail.com>
2022-02-16 12:12:31 -0300

    create database migrations and entitties

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -8,8 +8,10 @@ interface IRequest {
 class CreateCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    execute({ name, description }: IRequest): void {
-        const categoryAlreadyExist = this.categoriesRepository.findByName(name);
+    async execute({ name, description }: IRequest): Promise<void> {
+        const categoryAlreadyExist = await this.categoriesRepository.findByName(
+            name
+        );
 
         if (categoryAlreadyExist) {
             throw new Error("Category already exists!");
