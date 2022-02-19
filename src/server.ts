d0b88d181f5f5c10a30d015eccd684f8f5c7e853
Gustavo Aragão <gustavoma_@hotmail.com>
2022-02-19 11:58:04 -0300

    fix error what occurs when changed paths to a shortcut type

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 import "./database";
-import "./shared/container";
+import "@shared/container";
 
 const app = express();
 
