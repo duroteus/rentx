b92fcbf8fb516788662efb9b125f373e314a0755
Gustavo Aragão <gustavoma_@hotmail.com>
2022-02-23 12:17:50 -0300

    create integration test of list and create categories

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,71 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuidv4();
+        const password = await hash("papaehpop", 8);
+
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+        values('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', '123321')`
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("should be able to create a new category", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com",
+            password: "papaehpop",
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post("/categories")
+            .send({
+                name: "Category Supertest",
+                description: "Category Supertest",
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("shouldn't be able to create a new category with an already existing name", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com",
+            password: "papaehpop",
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post("/categories")
+            .send({
+                name: "Category Supertest",
+                description: "Category Supertest",
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(400);
+    });
+});
