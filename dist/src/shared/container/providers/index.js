3000e2310ff0342b463b073d9a63bf8beb013851
Gustavo <gustavo.kjr@gmail.com>
2022-03-04 11:45:57 -0300

    production configuration

--- dist/src/shared/container/providers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-const DayjsDateProvider_1 = require("./DateProvider/implementations/DayjsDateProvider");
-tsyringe_1.container.registerSingleton("DayjsDateProvider", DayjsDateProvider_1.DayjsDateProvider);
--- dist/src/shared/infra/http/server.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen(3333, () => console.log("Server is running on PORT 3333 (http://localhost:3333) !"));
