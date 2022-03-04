9d1ad3ad48ca035a4a8ffd693d1903cfd2200863
Gustavo <gustavo.kjr@gmail.com>
2022-03-04 12:24:20 -0300

    alter server is running message

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,3 @@
 import { app } from "./app";
 
-app.listen(3333, () =>
-    console.log("Server is running on PORT 3333 (http://localhost:3333) !")
-);
+app.listen(3333, () => console.log("Server is running!"));
