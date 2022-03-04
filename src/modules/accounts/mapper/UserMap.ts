07949ba14e2bfab690e378bad8c6ba2ed78b2783
Gustavo <gustavo.kjr@gmail.com>
2022-03-04 08:11:41 -0300

    create mapper for user profile

---
+++ src/modules/accounts/mapper/UserMap.ts
@@ -0,0 +1,28 @@
+import { instanceToInstance } from "class-transformer";
+
+import { IUserResponseDTO } from "../dtos/IUserResponseDTO";
+import { User } from "../infra/typeorm/entities/User";
+
+class UserMap {
+    static toDTO({
+        id,
+        email,
+        name,
+        driver_license,
+        avatar,
+        avatar_url,
+    }: User): IUserResponseDTO {
+        const user = instanceToInstance({
+            id,
+            email,
+            name,
+            driver_license,
+            avatar,
+            avatar_url,
+        });
+
+        return user;
+    }
+}
+
+export { UserMap };
--- src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { IUserResponseDTO } from "@modules/accounts/dtos/IUserResponseDTO";
+import { UserMap } from "@modules/accounts/mapper/UserMap";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 @injectable()
@@ -9,10 +10,10 @@ class ProfileUserUseCase {
         @inject("UsersRepository")
         private usersRepository: IUsersRepository
     ) {}
-    async execute(id: string): Promise<User> {
+    async execute(id: string): Promise<IUserResponseDTO> {
         const user = await this.usersRepository.findById(id);
 
-        return user;
+        return UserMap.toDTO(user);
     }
 }
 
